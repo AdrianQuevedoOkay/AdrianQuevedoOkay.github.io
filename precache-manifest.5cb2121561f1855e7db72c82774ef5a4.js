e0b852c5a58871b68c44a23c2eccd5fbfa58255c
AdrianQ2x <adrianquevedo@yahoo.com>
2023-01-20 11:13:19 -0300

    Updates

---
+++ precache-manifest.5cb2121561f1855e7db72c82774ef5a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "79cb93f48e711d82c72e",
+    "url": "/github.io/robofriends/static/css/main.b8951004.chunk.css"
+  },
+  {
+    "revision": "79cb93f48e711d82c72e",
+    "url": "/github.io/robofriends/static/js/main.79cb93f4.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/github.io/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "dbe44099fd26b9f21f4f",
+    "url": "/github.io/robofriends/static/css/2.1c9f802a.chunk.css"
+  },
+  {
+    "revision": "dbe44099fd26b9f21f4f",
+    "url": "/github.io/robofriends/static/js/2.dbe44099.chunk.js"
+  },
+  {
+    "revision": "6065c8b3b646c3d0b8cb",
+    "url": "/github.io/robofriends/static/js/runtime~main.6065c8b3.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/github.io/robofriends/static/media/SEGA.82e09f78.ttf"
+  },
+  {
+    "revision": "9a627f4ec2122b6c6a0bd3f7cefce3ca",
+    "url": "/github.io/robofriends/index.html"
+  }
+];
